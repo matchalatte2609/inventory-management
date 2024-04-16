90afd46956f64ee9d22cf01681fff23970ca4de9
Marco Lai <87260485+laithiennhan@users.noreply.github.com>
2024-04-15 23:14:51 -0400

    remove redundant comments in Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,38 +1,3 @@
-// import "./Login.css"
-
-// const login = () => {
-//     return {}
-//     var email = document.getElementById('email').value;
-//     var password = document.getElementById('password').value;
-//     var errorMessage = document.getElementById('error-message');
-  
-//     // Validate email and password
-//     if (email.endsWith("@tierra.vn") && password === "01062016") {
-//       // Success, redirect to dashboard or do something else
-//       errorMessage.textContent = '';
-//       console.log('Login successful');
-//       // For demonstration purposes, otherwise you'd redirect the user
-//       alert('Login successful!');
-//     } else {
-//       // Failed, show error message
-//       errorMessage.textContent = 'Invalid email or password.';
-//     }
-//   }
-
-// //   <div class="login-container">
-// //   <div id="error-message" class="error-message"></div>
-// //   <input type="email" id="email" class="form-input" placeholder="Email" required>
-// //   <input type="password" id="password" class="form-input" placeholder="Password" required>
-// //   <button id="login-button" class="login-button">Log In</button>
-// // </div>  
-
-//   // Add event listener for the login button
-//   document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById('login-button').addEventListener('click', login);
-//   });
-
-//   export default login;
-
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import './Login.css';
